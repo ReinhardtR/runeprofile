d5dce72d325e454186012291d7885fc50c2073d7
Reinhardt Rijna <reinhardtrijna@gmail.com>
2022-07-02 16:34:40 +0200

    remove dist from db

--- packages/db/dist/client.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Instantiates a single instance PrismaClient and save it on the global object.
- * @link https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices
- */
-import { PrismaClient } from "@prisma/client";
-export declare const prisma: PrismaClient;
-//# sourceMappingURL=client.d.ts.map
