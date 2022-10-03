c93ed0d24a5613eb3f7e13a3750a3c546b973433
Reinhardt Rijna <reinhardtrijna@gmail.com>
2022-10-03 21:10:29 +0200

    Update env.js

--- src/server/env.js
+++ src/server/env.js
@@ -3,13 +3,13 @@
  * This file is included in `/next.config.js` which ensures the app isn't built with invalid env vars.
  * It has to be a `.js`-file to be imported there.
  */
-/* eslint-disable @typescript-eslint/no-var-requires */
 const { z } = require("zod");
 
-/*eslint sort-keys: "error"*/
 const envSchema = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]),
+
   DATABASE_URL: z.string().url(),
+
   FIREBASE_API_KEY: z.string(),
   FIREBASE_PROJECT_ID: z.string(),
   FIREBASE_APP_ID: z.string(),
