9fdaecf5fbfaea7467b47c6da4a84128900188fe
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2024-04-02 14:57:07 +0200

    remove profile on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import { getProfilFullWithHash } from "~/lib/data/get-profile";
-
 export default async function Page() {
-  const data = await getProfilFullWithHash({
-    accountHash: "123",
-  });
-
   return (
     <div className="text-white">
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <pre>Home</pre>
     </div>
   );
 }
