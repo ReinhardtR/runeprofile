a601538083fefd54b25a487c70939fe26e8977f5
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2024-04-08 23:22:52 +0200

    add changelog meta data

--- src/app/info/change-log/layout.tsx
+++ src/app/info/change-log/layout.tsx
@@ -2,10 +2,16 @@ import { Layout } from "~/components/layout";
 
 export default function ChangeLogLayout(props: { children: React.ReactNode }) {
   return (
-    <Layout>
-      <div className="container prose pb-32 dark:prose-invert lg:prose-lg prose-headings:text-primary-foreground prose-h1:text-primary prose-em:text-muted-foreground prose-img:my-0">
-        {props.children}
-      </div>
-    </Layout>
+    <>
+      <head>
+        <title>Change Log | RuneProfile</title>
+        <meta name="description" content="Change log for RuneProfile." />
+      </head>
+      <Layout>
+        <div className="container prose pb-32 dark:prose-invert lg:prose-lg prose-headings:text-primary-foreground prose-h1:text-primary prose-em:text-muted-foreground prose-img:my-0">
+          {props.children}
+        </div>
+      </Layout>
+    </>
   );
 }
