ad8ff628c4de991ba1de4d4b40a9a644a4e1fb4c
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2024-04-08 23:19:16 +0200

    add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import {
 } from "next/font/google";
 import localFont from "next/font/local";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { Providers } from "~/app/Providers";
 
@@ -61,6 +62,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
           {props.children}
         </Providers>
         <TailwindIndicator />
+        <SpeedInsights />
         <Analytics />
       </body>
     </html>
