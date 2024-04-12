882b8c723bfb55e494965526c0ecdf5fc5821da1
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2024-04-12 20:31:10 +0200

    fix hydration mismatch error

--- src/app/info/change-log/layout.tsx
+++ src/app/info/change-log/layout.tsx
@@ -1,17 +1,18 @@
+import { Metadata } from "next";
+
 import { Layout } from "~/components/layout";
+
+export const metadata: Metadata = {
+  title: "Change Log | RuneProfile",
+  description: "Change log for RuneProfile.",
+};
 
 export default function ChangeLogLayout(props: { children: React.ReactNode }) {
   return (
-    <>
-      <head>
-        <title>Change Log | RuneProfile</title>
-        <meta name="description" content="Change log for RuneProfile." />
-      </head>
-      <Layout>
-        <div className="container prose pb-32 dark:prose-invert lg:prose-lg prose-headings:text-primary-foreground prose-h1:text-primary prose-em:text-muted-foreground prose-img:my-0">
-          {props.children}
-        </div>
-      </Layout>
-    </>
+    <Layout>
+      <div className="container prose pb-32 dark:prose-invert lg:prose-lg prose-headings:text-primary-foreground prose-h1:text-primary prose-em:text-muted-foreground prose-img:my-0">
+        {props.children}
+      </div>
+    </Layout>
   );
 }
