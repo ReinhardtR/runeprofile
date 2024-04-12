da433fd4196de195138b98ad6405d0d65dd0379d
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2024-04-12 20:22:06 +0200

    add new hiscores leaderboards

--- src/lib/constants/gamemodes.ts
+++ src/lib/constants/gamemodes.ts
@@ -3,6 +3,8 @@ import { HiscoresGameMode } from "~/lib/domain/profile-data-types";
 export const GAME_MODE_DISPLAY_TEXT = {
   NORMAL: "Normal",
   IRONMAN: "Ironman",
-  HARDCORE: "Hardcore Ironman",
-  ULTIMATE: "Ultimate Ironman",
+  HARDCORE_IRONMAN: "Hardcore Ironman",
+  ULTIMATE_IRONMAN: "Ultimate Ironman",
+  "1_DEFENCE_PURE": "1 Defence Pure",
+  LEVEL_3_SKILLER: "Level 3 Skiller",
 } satisfies Record<HiscoresGameMode, string>;
