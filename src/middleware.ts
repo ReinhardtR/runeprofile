5139e3ef432256448b5eaa2cbbbcd4eb936e73e9
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2024-05-11 21:38:34 +0200

    disable ratelimit analytics

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,7 +12,6 @@ const redis = new Redis({
 const ratelimit = new Ratelimit({
   redis,
   limiter: Ratelimit.slidingWindow(5, "5 s"),
-  analytics: true,
 });
 
 export default async function middleware(
