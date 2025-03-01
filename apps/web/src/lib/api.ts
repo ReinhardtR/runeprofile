38bdc21f4e245cf6accb3bf7fd77213bb9b02fa1
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2025-03-01 15:14:08 +0100

    setup api url in web

--- apps/web/src/lib/api.ts
+++ apps/web/src/lib/api.ts
@@ -1,4 +1,4 @@
 import { hc } from "hono/client";
 import type { ApiType } from "@runeprofile/api";
 
-export const api = hc<ApiType>("http://localhost:8787");
+export const api = hc<ApiType>(import.meta.env.VITE_API_URL);
